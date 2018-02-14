773f8fd97f21e1565067d95f9561fc555628df21
arturazarov <artur.azarau@gmail.com>
2018-02-14 12:21:53 +0300

    Project created

---
+++ Happy Days/View Controllers/RootViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  Happy Days
+//
+//  Created by Артур Азаров on 14.02.2018.
+//  Copyright © 2018 Артур Азаров. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+}
+
