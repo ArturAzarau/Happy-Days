ab9e0c5073657696ae5afba0e9a6063e95db7f14
arturazarov <artur.azarau@gmail.com>
2018-02-14 13:09:46 +0300

    UI was created

---
+++ Happy Days/UIKit/CollectionViewCell/MemoryCell.swift
@@ -0,0 +1,16 @@
+//
+//  MemoryCell.swift
+//  Happy Days
+//
+//  Created by Артур Азаров on 14.02.2018.
+//  Copyright © 2018 Артур Азаров. All rights reserved.
+//
+
+import UIKit
+
+final class MemoryCell: UICollectionViewCell {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
---
+++ Happy Days/View Controllers/PermissionsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Happy Days
+//
+//  Created by Артур Азаров on 14.02.2018.
+//  Copyright © 2018 Артур Азаров. All rights reserved.
+//
+
+import UIKit
+
+final class PermissionsViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var helpLabel: UILabel!
+    
+    
+    // MARK: - Actions
+    @IBAction func requestPermissions(_ sender: UIButton) {
+        
+    }
+}
+
