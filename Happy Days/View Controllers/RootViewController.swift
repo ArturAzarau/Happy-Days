7dd2191133ddaf20eea1cdee2da267ebc90bd6c5
arturazarov <artur.azarau@gmail.com>
2018-02-14 15:16:11 +0300

    - Loading of images - Writing memories to files - collection view data source

--- Happy Days/View Controllers/RootViewController.swift
+++ Happy Days/View Controllers/RootViewController.swift
@@ -13,10 +13,14 @@ import Speech
 
 private enum ReuseIdentifier {
     static let cell = "Memory"
-    static let section = "Header"
+    static let header = "Header"
 }
 
 final class RootViewController: UICollectionViewController {
+    
+    // MARK: - Properties
+    
+    var memories = [URL]()
     
     // MARK: - Life cycle
     override func viewDidAppear(_ animated: Bool) {
@@ -28,8 +32,7 @@ final class RootViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: ReuseIdentifier.cell)
+        loadMemories()
     }
 
     // MARK: - Methods
@@ -47,25 +50,158 @@ final class RootViewController: UICollectionViewController {
         }
     }
     
-    // MARK: UICollectionViewDataSource
-
+    // MARK: -
+    
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    // MARK: -
+    
+    private func loadMemories() {
+        memories.removeAll()
+        
+        guard let files = try? FileManager.default.contentsOfDirectory(at: getDocumentsDirectory(), includingPropertiesForKeys: nil, options: []) else { return }
+        
+        for file in files {
+            let fileName = file.lastPathComponent
+            if fileName.hasSuffix(".thumb") {
+                let noExtension = fileName.replacingOccurrences(of: ".thumb", with: "")
+                let memoryPath = getDocumentsDirectory().appendingPathComponent(noExtension)
+                memories.append(memoryPath)
+            }
+        }
+        collectionView?.reloadSections(IndexSet(integer: 1))
+    }
+    
+    // MARK: -
+    
+    private func saveNewMemory(image: UIImage) {
+        let memoryName = "memory- \(Date().timeIntervalSince1970)"
+        let imageName = memoryName + ".jpg"
+        let thumbnailImage = memoryName + ".thumb"
+        
+        do {
+            let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+            if let jpegData = UIImageJPEGRepresentation(image, 80) {
+                try jpegData.write(to: imagePath, options: [.atomicWrite])
+            }
+            
+            if let thumbnail = resize(image: image, to: 200) {
+                let imagePath = getDocumentsDirectory().appendingPathComponent(thumbnailImage)
+                if let jpegData = UIImageJPEGRepresentation(thumbnail, 80) {
+                    try jpegData.write(to: imagePath, options: [.atomicWrite])
+                }
+                
+            }
+        } catch {
+            print("Failed to save to disk")
+        }
+    }
+    
+    // MARK: -
+    
+    private func resize(image: UIImage, to width: CGFloat) -> UIImage? {
+        let scale = width / image.size.width
+        let height = image.size.height * scale
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: height) , false, 0)
+        image.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+        let vc = UIImagePickerController()
+        vc.modalPresentationStyle = .formSheet
+        vc.delegate = self
+        navigationController?.present(vc, animated: true)
+    }
+    
+    // MARK: - Collection view data source
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
-
-
+    
+    // MARK: -
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        if section == 0 {
+            return 0
+        } else {
+            return memories.count
+        }
     }
-
+    
+    // MARK: -
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ReuseIdentifier.cell, for: indexPath)
-    
-        // Configure the cell
-    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ReuseIdentifier.cell, for: indexPath) as! MemoryCell
+        
+        let memory = memories[indexPath.row]
+        let imageName = thumbnailURL(for: memory).path
+        let image = UIImage(contentsOfFile: imageName)
+        cell.imageView.image = image
+        
         return cell
     }
-
+    
+    // MARK: -
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        return collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: ReuseIdentifier.header, for: indexPath)
+    }
+    
+    // MARK: - Helper methods
+    
+    func imageURL(for memory: URL) -> URL {
+        return memory.appendingPathExtension("jpg")
+    }
+    
+    // MARK: -
+    
+    func thumbnailURL(for memory: URL) -> URL {
+        return memory.appendingPathExtension("thumb")
+    }
+    
+    // MARK: -
+    
+    func audioURL(for memory: URL) -> URL {
+        return memory.appendingPathExtension("m4a")
+    }
+    
+    // MARK: -
+    
+    func transcriptionURL(for memory: URL) -> URL {
+        return memory.appendingPathExtension("txt")
+    }
 }
+
+extension RootViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true)
+        
+        if let possibleImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            saveNewMemory(image: possibleImage)
+            loadMemories()
+        }
+    }
+}
+
+extension RootViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        if section == 1 {
+            return CGSize.zero
+        } else {
+            return CGSize(width: 0, height: 50)
+        }
+    }
+}
+
+extension RootViewController: UINavigationControllerDelegate {}
